4013ffbccc7c101abfcf7986e0bde7db3f2385aa
Mohammad Huzaifa <mhganie687@gmail.com>
2026-09-01 21:49:19 +0530

    add home page to build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/CHAOX-TAB/',
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        home: resolve(__dirname, 'home.html')
+      }
+    }
+  }
 })
